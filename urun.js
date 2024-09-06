44b64c3953f697ac36a0456e91f37088b27cf7e5
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-09-06 15:55:16 -0400

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,6 +1,6 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
-	if (e.key == "!" && e.ctrlKey) {
+	if (e.key === "!" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
 		(e.src = "//topg916.github.io/uRun/popup.html"),
@@ -10,9 +10,10 @@ document.addEventListener("keydown", function (e) {
 			t.addEventListener("message", function (e) {
 				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
-	} else if (e.key == "~" && e.ctrlKey) {
+	} else if (e.key === "~" && e.ctrlKey) {
 		fetch("topg916.github.io/scripts/scripts/kill.js").then(r => r.text()).then(r => eval(r));
-	} else if (e.key == "`" && e.ctrlKey) {
+	} else if (e.key === "`" && e.ctrlKey) {
 		fetch("topg916.github.io/scripts/aboutblanker/launch.js").then(r => r.text()).then(r => eval(r));
+		console.log("trying guys");
 	}
 });
