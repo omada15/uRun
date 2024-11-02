c15a70b237fa561a702e6453644e6ae30c1ec74c
Omada <137461829+omada15@users.noreply.github.com>
2024-11-02 11:24:52 -0400

    Update urun.js

--- urun.js
+++ urun.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "!" && e.ctrlKey) {
        var t = window.open("", "_blank", "width=500,height=300");
        var e = t.document.createElement("iframe");
-       (e.src = "//topg916.github.io/uRun/popup.html"),
+       (e.src = "//omada15.github.io/uRun/popup.html"),
           (e.style.cssText = "width:100%; height:100%; border:none;"),
           t.document.body.appendChild(e),
           t.document.title = "uRun",
@@ -11,7 +11,7 @@ document.addEventListener("keydown", function (e) {
              e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
           });
     } else if (e.key === "@" && e.ctrlKey) {
-       fetch("https://topg916.github.io/scripts/aboutblanker/launch.js").then(r => r.text()).then(r => eval(r));
+       fetch("https://omada15.github.io/scripts/ab/launch.js").then(r => r.text()).then(r => eval(r));
     } else if (e.key === "#" && e.ctrlKey) {
         eval(prompt("this"));
     }
