d4c41442b42cb742aee786b1b36f3c2762ccd05d
Omada <137461829+omada15@users.noreply.github.com>
2024-11-14 21:01:58 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,6 +1,6 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
-    if (e.key === "!" && e.ctrlKey) {
+    if (e.key === "!" && e.ctrlKey) {//urun
        var t = window.open("", "_blank", "width=500,height=300");
        var e = t.document.createElement("iframe");
        (e.src = "//omada15.github.io/uRun/popup.html"),
@@ -10,9 +10,15 @@ document.addEventListener("keydown", function (e) {
           t.addEventListener("message", function (e) {
              e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
           });
-    } else if (e.key === "@" && e.ctrlKey) {
+    } else if (e.key === "@" && e.ctrlKey) {//ab
        fetch("https://omada15.github.io/scripts/ab/launch.js").then(r => r.text()).then(r => eval(r));
-    } else if (e.key === "#" && e.ctrlKey) {
+    } else if (e.key === "#" && e.ctrlKey) {//eval
         eval(prompt("this"));
+    }
+    if (window.location.hostname === "leetcode.com") {//lcs
+        let fb = document.createElement("script");
+        fb.type = "module";
+        fb.src = "https://omada15.github.io/leetcodeSave/index.js";
+        document.body.appendChild(fb);
     }
 });
