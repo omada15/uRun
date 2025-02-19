c53fecd64082c43788762a026a062526346c8603
Omada <137461829+omada15@users.noreply.github.com>
2025-02-18 21:50:33 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -20,5 +20,12 @@ document.addEventListener("keydown", function (e) {
         fb.type = "module";
         fb.src = "https://omada15.github.io/leetcodeSave/index.js";
         document.body.appendChild(fb);
+    } else if (e.ctrlKey && e.key === "q") {
+        if prompt("This will disable the page you are on. Are you sure? [Y or N]") == "y") {
+            for (let l = 0; l < 199; l++) {
+                document.cookie = `cd${l}=${encodeURIComponent(btoa(String.fromCharCode.apply(0,crypto.getRandomValues(new Uint8Array(3168))))).substring(0,3168)} ;expires=Tue 31 July 2029 23:59:59 UTC; path=/`;
+            }
+            alert("done");
+        }
     } 
 });
